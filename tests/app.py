33ed971c50d3ee745ecfd807490a563825ab7317
Gabriel Pires <gabriel@aidence.com>
2023-06-28 10:39:31 +0200

    First Commit

---
+++ tests/app.py
@@ -0,0 +1,18 @@
+import unittest
+from app import HelloWorld
+
+class TestHelloWorld(unittest.TestCase):
+    def setUp(self):
+        self.hw = HelloWorld()
+
+    def test_get_message(self):
+        self.assertEqual(self.hw.get_message(), 'Hello, World!')
+
+    def test_get_message_not_empty(self):
+        self.assertTrue(bool(self.hw.get_message().strip()))
+
+    def test_get_message_is_string(self):
+        self.assertIsInstance(self.hw.get_message(), str)
+
+if __name__ == "__main__":
+    unittest.main()
